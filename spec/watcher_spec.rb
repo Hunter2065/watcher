ac37835ab9def2bbb318a85fc60cab9efc179fee
Hunter2065 <vanuha277@gmail.com>
2022-01-11 11:27:49 +0200

    spec(watcher_spec.rb) TDD for uploading files

--- spec/watcher_spec.rb
+++ spec/watcher_spec.rb
@@ -8,7 +8,7 @@ describe Watcher do
 
   describe '#methods' do
     context 'with :start_directory_monitoring' do
-      it 'should be successful' do
+      it 'should be successful for DIRECTORY' do
         # triggered event :create
         expect_any_instance_of(Filewatcher).to receive(:watch).and_yield('root/path/to/parent/new_folder', :created)
 
@@ -19,6 +19,24 @@ describe Watcher do
         expect(ServerClient).to receive(:post).with(
           type: 'DIRECTORY',
           relative_path: 'parent/new_folder'
+        ).and_return(OpenStruct.new(code: 200))
+
+        watcher.start_directory_monitoring
+      end
+
+      it 'should be successful for FILE' do
+        # triggered event :create
+        expect_any_instance_of(Filewatcher).to receive(:watch).and_yield('root/path/to/parent/file.txt', :created)
+
+        # by path getted details
+        expect_any_instance_of(PathTool).to receive(:type).and_return('file')
+
+        # send to server
+        allow(File).to receive(:new).and_return('file.txt')
+        expect(ServerClient).to receive(:post).with(
+          type: 'FILE',
+          relative_path: 'parent/file.txt',
+          attachment: 'file.txt'
         ).and_return(OpenStruct.new(code: 200))
 
         watcher.start_directory_monitoring

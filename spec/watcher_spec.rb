4b139b93eab132f672f0d8eb6c22e4509b392a37
Hunter2065 <vanuha277@gmail.com>
2022-01-12 15:39:40 +0200

    spec(watcher.rb) add handling event :deleted

--- spec/watcher_spec.rb
+++ spec/watcher_spec.rb
@@ -7,39 +7,63 @@ describe Watcher do
   let(:watcher) { described_class.new('root/path/to/parent') }
 
   describe '#methods' do
-    context 'with :start_directory_monitoring' do
-      it 'should be successful for DIRECTORY' do
-        # triggered event :create
-        expect_any_instance_of(Filewatcher).to receive(:watch).and_yield('root/path/to/parent/new_folder', :created)
+    context ':start_directory_monitoring' do
+      context 'when action is :create' do
+        it 'should be successful for DIRECTORY' do
+          # triggered event :create
+          expect_any_instance_of(Filewatcher).to receive(:watch).and_yield(
+            'root/path/to/parent/new_folder',
+            :created
+          )
 
-        # by path getted details
-        expect_any_instance_of(PathTool).to receive(:type).and_return('directory')
+          # by path getted details
+          expect_any_instance_of(PathTool).to receive(:type).and_return('directory')
 
-        # send to server
-        expect(ServerClient).to receive(:post).with(
-          type: 'DIRECTORY',
-          relative_path: 'parent/new_folder'
-        ).and_return(OpenStruct.new(code: 200))
+          # send to server
+          expect(ServerClient).to receive(:post).with(
+            type: 'DIRECTORY',
+            relative_path: 'parent/new_folder'
+          ).and_return(OpenStruct.new(code: 200))
 
-        watcher.start_directory_monitoring
+          watcher.start_directory_monitoring
+        end
+
+        it 'should be successful for FILE' do
+          # triggered event :create
+          expect_any_instance_of(Filewatcher).to receive(:watch).and_yield('root/path/to/parent/file.txt', :created)
+
+          # by path getted details
+          expect_any_instance_of(PathTool).to receive(:type).and_return('file')
+
+          # send to server
+          allow(File).to receive(:new).and_return('file.txt')
+          expect(ServerClient).to receive(:post).with(
+            type: 'FILE',
+            relative_path: 'parent/file.txt',
+            attachment: 'file.txt'
+          ).and_return(OpenStruct.new(code: 200))
+          watcher.start_directory_monitoring
+        end
       end
 
-      it 'should be successful for FILE' do
-        # triggered event :create
-        expect_any_instance_of(Filewatcher).to receive(:watch).and_yield('root/path/to/parent/file.txt', :created)
+      context 'when action is :deleted' do
+        context 'for DIRECTORY' do
+          it 'should be successful for DIRECTORY' do
+            # triggered event :deleted
+            expect_any_instance_of(Filewatcher).to receive(:watch).and_yield(
+              'root/path/to/parent/test_folder',
+              :deleted
+            )
 
-        # by path getted details
-        expect_any_instance_of(PathTool).to receive(:type).and_return('file')
+            # get relative path
+            #send to server
+            expect(ServerClient).to receive(:delete).with(
+              relative_path: 'parent/test_folder'
+            ).and_return(OpenStruct.new(code: 200))
 
-        # send to server
-        allow(File).to receive(:new).and_return('file.txt')
-        expect(ServerClient).to receive(:post).with(
-          type: 'FILE',
-          relative_path: 'parent/file.txt',
-          attachment: 'file.txt'
-        ).and_return(OpenStruct.new(code: 200))
-
-        watcher.start_directory_monitoring
+            watcher.start_directory_monitoring
+          end
+        end
       end
     end
   end

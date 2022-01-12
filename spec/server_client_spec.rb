cd032121f87ff877997f90a1f59f57016fa0ad18
Hunter2065 <vanuha277@gmail.com>
2022-01-12 15:37:17 +0200

    spec(server_client.rb) TDD add action delete

--- spec/server_client_spec.rb
+++ spec/server_client_spec.rb
@@ -1,14 +1,26 @@
 require 'spec_helper'
+require 'ostruct'
 require_relative '../bin/server_client'
 
 describe ServerClient do
   describe '#methods' do
     context 'with :post' do
       it 'should have succesful response' do
-        allow(described_class).to receive(:post).and_return(200)
-        expect(
-          described_class.post(type: 'folder', relative_path: 'path/to/folder')
-        ).to eq(200)
+        allow(described_class).to receive(:post).and_return(OpenStruct.new(code: 200))
+
+        response = described_class.post(type: 'folder', relative_path: 'path/to/folder')
+
+        expect(response.code).to eq(200)
+      end
+    end
+
+    context 'with :delete' do
+      it 'should have successful response' do
+        allow(described_class).to receive(:delete).and_return(OpenStruct.new(code: 200))
+
+        response = described_class.delete(relative_path: 'path/to/folder')
+
+        expect(response.code).to eq(200)
       end
     end
   end
